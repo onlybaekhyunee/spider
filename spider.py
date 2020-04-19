0715275486ee0949babfb12eb24b30f9b83f501a
onlybaekhyunee <olbaekhyunee@163.com>
2020-04-19 23:54:13 +0800

    update spider

--- spider.py
+++ spider.py
@@ -5,6 +5,10 @@ import gzip
 import re
 
 class Spider():
+    """  
+    2020-4-19完结
+    原始爬虫小程序：以斗鱼网站为例 
+    """
     url = 'https://www.douyu.com/g_DOTA2'
     # url = 'https://www.huya.com/g/dota2'
     root_pattern = '<div class="DyListCover-info">(.*?)</div>'
@@ -12,8 +16,10 @@ class Spider():
     user_pattern = '<h2 class="DyListCover-user.*?/svg>(.*?)</h2>'
     
     def __fetch_content(self,r):
-        # 两个下划线开头，私有方法  
-        # 获取请求到的html代码     
+        """ 
+        解析从服务器请求到的数据包，获取html代码 
+        两个下划线开头，私有方法 
+        """     
         encoding = r.info().get('Content-Encoding')
         encodetype = r.info().get('Content-Type')
         content = r.read()
@@ -41,7 +47,9 @@ class Spider():
             return False
 
     def __analysis(self,htmls):
-        # html代码数据分析，利用唯一标签识别数据
+        """ 
+        html代码数据分析，利用唯一标签识别数据
+        """
         name_pops = []
         root_html = re.findall(Spider.root_pattern,htmls)
         for d in root_html:
@@ -53,7 +61,9 @@ class Spider():
         return name_pops
 
     def __refine(self,anchors):
-        # 数据精炼
+        """ 
+        数据精炼：去除list的方括号，空格等
+        """
         l = lambda anchor: {
             'Name':anchor['Name'][0].strip(),
             'PopularityValues':anchor['PopularityValues'][0].strip()
@@ -62,11 +72,16 @@ class Spider():
         return list(m)
 
     def __sort(self,anchors):
+        """ 
+        数据排序，使用的内置函数sorted
+        """
         anchors = sorted(anchors,key=self.__sort_seed,reverse=True)
         return anchors
 
     def __sort_seed(self,anchor):
-        # 排序基准
+        """ 
+        排序基准设置
+        """
         rn = re.findall('\d+\.?\d*',anchor['PopularityValues'])
         num = float(rn[0])
         if '万' in anchor['PopularityValues']:
@@ -74,15 +89,23 @@ class Spider():
         return num
 
     def __show(self,anchors):
-        for a in anchors:
-            print('%-20s'%a['Name'] + '------' + a['PopularityValues'])
+        """ 
+        格式化展示数据结果
+        """
+        for rank in range(0,len(anchors)):
+            # print('%-20s'%a['Name'] + '------' + a['PopularityValues'])
+            print('rank  '+str(rank+1)
+            +':  '+anchors[rank]['Name']
+            +'('+anchors[rank]['PopularityValues']+')')
 
     def go(self):
-        # 入口方法
-        context = ssl._create_unverified_context()
-        # 取消证书签证
-        r = request.urlopen(Spider.url,context=context)
+        """ 
+        入口方法
+        """
+        # 取消证书验证
         # 打开https网站后，会验证SSL证书，当网站使用的不是CA证书，是自签名证书时会抛出URLError异常
+        context = ssl._create_unverified_context()      
+        r = request.urlopen(Spider.url,context=context)       
         htmls = self.__fetch_content(r)
         r.close()
         # 关闭连接，防止内存溢出
